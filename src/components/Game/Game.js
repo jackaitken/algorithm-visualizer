462d9e412569647ad48ba50e9a0503066fe08041
Jack Aitken <jaitk11@gmail.com>
2021-03-05 15:19:28 -0500

    Progress on board state & added title/instructions

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
 import { calculateWinner } from '../../helpers';
 import Board from '../Board/Board';
+import Square from '../Square/Square';
+import './Game.css'
 
 const Game = () => {
 
     const [board, setBoard] = useState(Array(2500).fill(null));
-    const [firstClick, setFirstClick] = useState(false);
-    const [secondClick, setSecondClick] = useState(false);
-    const winner = calculateWinner(board);
+    const [firstClick, setFirstClick] = useState();
+    const [secondClick, setSecondClick] = useState();
 
     const handleClick = (i) => {
+        const copyOfBoard = [...board]
+
         if (firstClick) {
             setSecondClick(true);
-            console.log('Second Click', i)
+            copyOfBoard[i] = '#'
+            setBoard(copyOfBoard)
+            console.log(copyOfBoard);
+
         } else {
             setFirstClick(true);
-            console.log('First Click', i)
+            copyOfBoard[i] = '#'
+            setBoard(copyOfBoard)
+            console.log(copyOfBoard);
         }
     }
 
-    const renderMoves = () => {
+    const renderClick = (i) => {
 
     }
 
-
     return (
-        <Board squares={board} onClick={handleClick} />
+        <>
+            <div className={'title'}>Pathfinding Visualizer</div>
+                <p className={'instructions'}>
+                First click on a starting square and then a click a
+                square to end on, then click "Begin" to find a path between 
+                the two.
+                </p>
+            <Board squares={board} onClick={handleClick} />
+        </>
     )
 }
 
