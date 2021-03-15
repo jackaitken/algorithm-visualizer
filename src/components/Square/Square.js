30fd858cde2a5a2b2d728b86e948ab12f2589011
Jack Aitken <jaitk11@gmail.com>
2021-03-15 16:35:51 -0400

    Added hover effects for squares

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,16 +1,8 @@
 import React from 'react'
-
-const style = {
-    border: '1px solid rgb(99, 101, 104)',
-    backgroundColor: 'rgb(188, 187, 186)',
-    fontSize: '1px',
-    color: 'rgba(0, 0, 0, 0)',
-    cursor: 'pointer',
-    outline: 'none',
-}
+import './Square.css'
 
 const Square = ({ value, onClick }) => (
-    <button id={'square'} style={style} onClick={onClick}>
+    <button id={'square'} className={'squareStyle'} onClick={onClick}>
     {value}
     </button>
 )
